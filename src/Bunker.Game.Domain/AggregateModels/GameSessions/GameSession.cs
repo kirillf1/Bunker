f0f9385e6fed6450dfbdb4b71261c4205bd6a7da
TaKi <TaKi@gendalf.ru>
2025-01-22 17:53:24 +0300

    added test properties

--- src/Bunker.Game.Domain/AggregateModels/GameSessions/GameSession.cs
+++ src/Bunker.Game.Domain/AggregateModels/GameSessions/GameSession.cs
@@ -1,8 +1,39 @@
-﻿namespace Bunker.Game.Domain.AggregateModels.GameSessions;
+﻿using Bunker.Game.Domain.AggregateModels.GameSessions.Characters;
+
+namespace Bunker.Game.Domain.AggregateModels.GameSessions;
 
 public class GameSession : Entity<Guid>, IAggregateRoot
 {
-    public GameSession(Guid id) : base(id)
+    public const byte MaxCharactersInGame = 12;
+
+    public const byte MinCharactersInGame = 5;
+
+    private List<Character> _characters;
+
+    public Bunkers.Bunker Bunker { get; private set; }
+
+    public Catastrophe Catastrophe { get; private set; }
+
+    public IReadOnlyCollection<Character> Characters => _characters;
+
+    public GameState GameState { get; private set; }
+
+    public string Name { get; private set; }
+
+    public byte FreeSeatsCount { get; private set; }
+
+    public void ChangeCharacteristic()
+    {
+
+    }
+
+    public void ChangeBunkerComponent()
+    {
+
+    }
+
+
+    private GameSession(Guid id) : base(id)
     {
     }
 }
---
+++ src/Bunker.Game.Domain/AggregateModels/GameSessions/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Bunker.Game.Domain.AggregateModels.GameSessions;
+
+public class Player : Entity<string>
+{
+    public string Name { get; private set; }
+
+    public Player(string id, string name) : base(id)
+    {
+        Name = name;
+    }
+
+}
