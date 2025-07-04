ae71a8be455f39242b14648042e016532ff22107
kirillf1 <kirillf2412@gmail.com>
2025-07-04 14:25:48 +0300

    added logging, telemetry

--- src/Bunker.ResultCreator.API/Program.cs
+++ src/Bunker.ResultCreator.API/Program.cs
@@ -1,38 +1,57 @@
-﻿using Bunker.ResultCreator.API.Extensions;
+﻿using Bunker.Infrastructure.Shared.Extensions;
+using Bunker.ResultCreator.API.Extensions;
+using Serilog;
+using Serilog.Events;
 
-namespace Bunker.ResultCreator.API;
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-public class Program
+try
 {
-    public static void Main(string[] args)
+    var builder = WebApplication.CreateBuilder(args);
+
+    var appName = "Bunker.ResultCreator.API";
+
+    // Telemetry
+    builder.AddBaseMetricsConfiguration(appName);
+    builder.AddBaseTracingConfiguration(appName);
+    builder.AddSerilogLogging(appName);
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddSwaggerGen(c => { });
+
+    builder.Services.AddOpenApi();
+
+    builder.Services.AddApplicationServices(builder.Configuration);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-
-        builder.Services.AddSwaggerGen(c => { });
-
-        builder.Services.AddOpenApi();
-
-        builder.Services.AddApplicationServices(builder.Configuration);
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    await app.RunAsync();
 }
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Server terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
+
+public partial class Program { }
